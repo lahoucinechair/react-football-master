e973a0b059a5a51bc4d11836918519c36a020dd2
lahoucinechair <46401740+lahoucinechair@users.noreply.github.com>
2020-11-24 18:12:47 +0100

    Updates

---
+++ precache-manifest.917a40928651f5ab1a1b1e38d6c45f35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "094b681d6f23a9df8602",
+    "url": "/react-football-master/static/css/main.9b014ca5.chunk.css"
+  },
+  {
+    "revision": "094b681d6f23a9df8602",
+    "url": "/react-football-master/static/js/main.886b546e.chunk.js"
+  },
+  {
+    "revision": "a07ce4115ffc8c39885a",
+    "url": "/react-football-master/static/js/runtime~main.a4ea4ebf.js"
+  },
+  {
+    "revision": "b9b5b1c6aa682e5a76df",
+    "url": "/react-football-master/static/js/2.2f513f4a.chunk.js"
+  },
+  {
+    "revision": "2be242902f8f7daeaf6ff1f729b60ad1",
+    "url": "/react-football-master/static/media/LaLiga.2be24290.png"
+  },
+  {
+    "revision": "3e055fb491632cd7be494535d08a13e1",
+    "url": "/react-football-master/static/media/Bundesliga.3e055fb4.png"
+  },
+  {
+    "revision": "0723988637eb0bcceb4564d8647b8599",
+    "url": "/react-football-master/static/media/Eredivisie.07239886.png"
+  },
+  {
+    "revision": "4b2af31f18df85fc286831937615225b",
+    "url": "/react-football-master/static/media/Ligue1.4b2af31f.png"
+  },
+  {
+    "revision": "2ea36f1c21b081213f172410bba0355b",
+    "url": "/react-football-master/static/media/PremierLeague.2ea36f1c.png"
+  },
+  {
+    "revision": "3ee74dc22ac21026487a24b49cf2ccfa",
+    "url": "/react-football-master/static/media/SerieA.3ee74dc2.png"
+  },
+  {
+    "revision": "557e926ca263b54b94ddfa22c2a1f99a",
+    "url": "/react-football-master/index.html"
+  }
+];
